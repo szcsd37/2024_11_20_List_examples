3d86741ed247bce86c2890dc76ab4a4c683a76c1
szcsd37 <szucs.dominik@diak.szbi-pg.hu>
2024-11-20 13:40:20 +0100

    add 01 and 02 solution

--- 01_lista_elemeinek_megszamolasa.py
+++ 01_lista_elemeinek_megszamolasa.py
@@ -4,3 +4,13 @@ Határozd meg a lista hosszát egy ciklus segítségével
 (a len() függvény megoldásán kívül használj for ciklusos megoldást is),
 és írd ki!
 """
+
+szamok = [5, 8, 12, 15, 22]
+
+szam_index = 1
+for szam in szamok:
+    print(szam_index)
+    szam_index += 1
+print()
+
+print(len(szamok))
